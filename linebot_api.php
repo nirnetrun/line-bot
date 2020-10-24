6ac32f5fb80163a9872812da2eeac46689085002
nirnetrun <ngamkerd@hotmail.com>
2020-10-24 17:04:54 +0700

    Update linebot_api.php

--- linebot_api.php
+++ linebot_api.php
@@ -1,6 +1,6 @@
 <?php
 
-$channelAccessToken = 'PZ6qlbYABvcIg+sly4KFcjs8rAVOW1+EEEDBgcOn86a9MwA+MNHV8//FPERaqcVuWnKEs4U+6oe0jLA++fQlGKdK9/SCRKlZ0x4otRbscQZBRbe5VDkXvu32iZAA+dpXEwrb47Ncr9kuH1vSp+t3LwdB04t89/1O/w1cDnyilFU='; // Access Token ค่าที่เราสร้างขึ้น
+$channelAccessToken = 'Zdp4Onz24qW4VEy7PdFv9Wfu0feiCMZn4GQTNmVKAmvLWHpms2y02ADeL7W3eSkDQSVrhN6cignE5yywDmTIXUEkwtJPBhxrHffiMjTAOLMThGWRzZ1DJ/TYR5oDlJH6ROJkA8nKqvbGLu1d5knohwdB04t89/1O/w1cDnyilFU='; // Access Token ค่าที่เราสร้างขึ้น
 
 $request = file_get_contents('php://input');   // Get request content
 
